dab9081627afd557c9d5774f82400b053b725211
sandrawg <105887486+sandrawg@users.noreply.github.com>
2022-06-28 10:51:35 +0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>CLASSIC THREADS BOUTIQUE</title>
+	<link rel="stylesheet" type="text/css" href="homepage.css">
+</head>
+<body>
+
+	<div class="banner">
+		<div class="navbar">
+			<div class="logo">
+				<img src="pictures/p16.jpeg" alt="">
+			</div>
+			<h2> CLASSIC THREADS BOUTIQUE</h2>
+				<nav>
+					<h5>
+		            <a href="#">HOME</a> 
+		            <a href="#">PROFILE</a>
+		            <a href="login.php">LOG IN /REGISTER</a>
+		            <a href="#">CART</a>
+		            </h5>
+		            <input type="text" placeholder="Search..">
+		        </nav>
+		    </div>
+		</div>
+
+	<div class="container">
+		<img src="pictures/p10.jpg" alt="">
+		<div class="text"> 
+			<h3>REFRESH YOUR WARDROBE!</h3>
+			<p>Shop in the season and look the iconinc look</p>
+			<div>
+			<button type="button">CLICK HERE TO SHOP NOW</button>
+		</div>
+		</div>
+	</div>
+
+	<div class="categories">
+		<h5>
+			<a href="#">MEN</a> 
+			<a href="#">WOMEN</a>
+			<a href="#">KIDS</a>
+			<a href="#">BABIES</a>
+			<a href="#">BRANDS</a>
+		</h5>
+	</div>
+
+	<div class="display">
+		<img src="pictures/p14.jpg" alt="">
+		<img src="pictures/p15.jpg" alt="">
+		<img src="pictures/p4.jpg" alt="">
+		<img src="pictures/p19.jpg" alt="">
+	</div>
+
+	<div class="containers">
+		<div class="box1">
+			<h4>WHO ARE WE?</h4><br><p>Classic Threads Boutique enables people, male or female, young or old, to design their own clothing whether they have any artistic abilities or not. It's fun, it's exciting, and it will be inexpensive for them. It will be fashion art that is unique and original. The consumer will be able to design any number of pieces to go with any other articles of clothing they own. Most of all they will be entertained while creating their own fashion art.</p>
+		</div>
+		<div class="box2">
+			<div class="texts">
+				<p>Shop the latest fashion, footwear and accessories from our exclusive brands</p>
+			</div>
+			<div>
+				<button type="button">SHOP NOW</button>
+			</div>
+		</div>
+	</div>
+
+	<div class="deals">
+		<h4><button type="button">>>>Some Deals Of The Day<<<</button></h4>
+	</div>
+
+	<div class="reviews">
+		<h3>REVIEWS</h3>
+		<div class="rev1">
+			<p><h1>“</h3>The best! Found me the perfect dress and such lovely service and attention<h1>“</h1><br>-Sarah Williams</p>
+		</div>
+		<div class="rev2">
+			<p><h1>“</h1>Very nice jeans, very comfortable with the stretch element and look good too<h1>”</h1><br>-Zora Ali</p>
+		</div>
+		<div class="rev3">
+			<p><h1>“</h1>I bought some shirts for two of my sons for Christmas, they love them<h1>”</h1><br>-Bob Gates</p>
+		</div>
+		<div class="button">
+			<button type="button">VIEW MORE>></button>
+	</div>
+
+	<footer class="footer">
+		<div class="container2">
+		
+		<div>	
+		<h5>LINKS
+		<ul>
+			<li><a href="#">Blogs</a></li>
+			<li><a href="#">Website</a></li>
+			<li><a href="#">Orders</a></li>
+			<li><a href="http://localhost/E-commerce%20clothes/loginregister.php">Log in/Sign in</a></li>
+		</ul></h5>
+	    </div>
+
+	    <div>
+		<h5>CUSTOMER SERVICE
+		<ul>
+			<li><a href="#">Terms & Conditions</a></li>
+			<li><a href="#">Contact Us</a></li>
+			<li><a href="#">FAQ'S</a></li>
+			<li><a href="#">Delivery</a></li>
+		</ul></h5>
+	    </div>
+
+	    <div>
+		<h5>CATEGORIES
+		<ul>
+			<li><a href="#">New Arrivals</a></li>
+			<li><a href="#">Men</a></li>
+			<li><a href="#">Women</a></li>
+			<li><a href="#">Kids</a></li>
+			<li><a href="#">Offers</a></li>
+		</ul></h5>
+	    </div>
+	    <div>
+		<h5>FOLLOW US ON
+		<ul>
+			<li><a href="#"><img src="pictures/icon1.png"></a></li>
+			<li><a href="#"><img src="pictures/icon2.jpg"></a></li>
+			<li><a href="#"><img src="pictures/icon3.png"></a></li>
+			<li><a href="#"><img src="pictures/icon5.png"></a></li>
+		</ul></h5>
+	        </div>
+	             </div>
+	       
+	     </footer>
+</body>
+</html>
